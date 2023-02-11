69d858794b46bfb7d281065bd44b285151fce57e
Jon McClure <jon.r.mcclure@gmail.com>
2023-02-11 12:46:12 +0000

    changes directly on main. adds release draft workflow and test

--- test/test.js
+++ test/test.js
@@ -5,4 +5,8 @@ describe('tests', function () {
   it('should do maths', function () {
     assert.equal(doMaths(2, 3), 5);
   });
+
+  it('should do more maths', function () {
+    assert.equal(doMaths(2, 2), 4);
+  });
 });
