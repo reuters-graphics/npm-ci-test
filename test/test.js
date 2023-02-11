9974a1900bd01b4b0e06e7d447c32ecf9c9f0c14
Jon McClure <jon.r.mcclure@gmail.com>
2023-02-11 13:18:11 +0000

    fixes test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-import { doDifferentMaths, doMaths } from '../index.js';
+import { doDifferentMaths, doMaths, doMaxMaths } from '../index.js';
 
 import assert from 'assert';
 
@@ -20,6 +20,6 @@ describe('tests', function () {
   });
 
   it('should do max maths', function () {
-    assert.equal(doDifferentMaths(4, 8), 1);
+    assert.equal(doMaxMaths(4, 8), 1);
   });
 });
